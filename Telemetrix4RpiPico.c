0ff617ed02f0b767febe021432dfc0fbe6cf46b6
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-06-08 14:37:46 +0200

    Add watchdog + i2c write timeout

--- Telemetrix4RpiPico.c
+++ Telemetrix4RpiPico.c
@@ -560,12 +560,11 @@ void i2c_write()
         i2c = i2c0;
     }
 
-    int i2c_sdk_call_return_value = i2c_write_blocking(i2c, (uint8_t)command_buffer[I2C_DEVICE_ADDRESS],
+    int i2c_sdk_call_return_value = i2c_write_blocking_until(i2c, (uint8_t)command_buffer[I2C_DEVICE_ADDRESS],
                                                        &(command_buffer[I2C_WRITE_BYTES_TO_WRITE]),
                                                        command_buffer[I2C_WRITE_NUMBER_OF_BYTES],
-                                                       (bool)command_buffer[I2C_WRITE_NO_STOP_FLAG]);
-
-    if (i2c_sdk_call_return_value == PICO_ERROR_GENERIC)
+                                                       (bool)command_buffer[I2C_WRITE_NO_STOP_FLAG], make_timeout_time_ms(100));
+    if (i2c_sdk_call_return_value != PICO_OK)
     {
         i2c_report_message[I2C_PACKET_LENGTH] = I2C_ERROR_REPORT_LENGTH; // length of the packet
         i2c_report_message[I2C_REPORT_ID] = I2C_WRITE_FAILED;            // report ID
@@ -1348,9 +1347,12 @@ int main()
     // starting afresh
     led_debug(2, 250);
 
+    watchdog_enable(1000, 1); // Add watchdog requiring trigger every 1s
+
     // infinite loop
     while (true)
     {
+        watchdog_update();
         get_next_command();
         if (!stop_reports)
         {
