0914a12eb9427269eb58b00956d6bd6064cb56df
Arend-Jan <ajvanhilten@student.tudelft.nl>
2023-09-13 16:20:05 +0200

    gps done!

--- src/Telemetrix4RpiPico.cpp
+++ src/Telemetrix4RpiPico.cpp
@@ -1217,7 +1217,6 @@ void sensor_new()
     return;
   }
   Sensor *sensor = nullptr;
-
   if (type == GPS)
   {
     sensor = new GPS_Sensor(sensor_data);
@@ -1259,35 +1258,44 @@ void readSensors()
 
 GPS_Sensor::GPS_Sensor(uint8_t settings[SENSORS_MAX_SETTINGS_A])
 {
-  // TODO: read uart, forward to serial
   auto rx = settings[0];
+  // auto tx = settings[1];
+  auto uart = settings[2];
   this->uart_id = uart0;
+  if (uart)
+  {
+    this->uart_id = uart1;
+  }
 
   const auto DATA_BITS = 8;
   const auto STOP_BITS = 1;
   const auto PARITY = UART_PARITY_NONE;
 
   uart_init(uart_id, 9600);
-  // gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART); // No need to send anything to the GPS receiver
+  // gpio_set_function(tx, GPIO_FUNC_UART); // No need to send anything to the GPS receiver
   gpio_set_function(rx, GPIO_FUNC_UART);
   uart_set_hw_flow(uart_id, false, false);
-
   // Set our data format
-  uart_set_format(uart_id, DATA_BITS, STOP_BITS, PARITY);
-
-  // Turn off FIFO's - we want to do this character by character
   uart_set_fifo_enabled(uart_id, true);
+
+  uart_set_format(uart_id, DATA_BITS, STOP_BITS, PARITY);
+  send_debug_info(100, 43);
 }
+
 void GPS_Sensor::readSensor()
 {
-  // TODO: is this fast enough? fifo is 32 bytes long, unknown speed of gps module
+  // Is this fast enough? fifo is 32 bytes long, unknown speed of gps module
   // baud = 9600 -> max ~960 bytes/s. Sensor interval is 100Hz, should be okay.
+
   std::vector<uint8_t> data;
-  while (uart_is_readable(this->uart_id))
+  if (uart_is_readable(this->uart_id))
   {
-    data.push_back(uart_getc(this->uart_id));
+    while (uart_is_readable(this->uart_id))
+    {
+      data.push_back(uart_getc(this->uart_id));
+    }
+    this->writeSensorData(data);
   }
-  this->writeSensorData(data);
 }
 
 ADXL345_Sensor::ADXL345_Sensor(uint8_t settings[SENSORS_MAX_SETTINGS_A])
