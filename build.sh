eee3c786ebfa16f120d25d0b79a75ab7738d02f2
Arend-Jan <a.j.vanhilten@tudelft.nl>
2023-12-06 09:59:27 +0100

    Speed up action with pre-generated container

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-cd /project/src/
+# cd /project/src/
 ls
 ls ..
 mkdir build2/
 ls
 cd build2
+# Sometimes the debug build reports more errors than Release
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make
+
+# Build for the artifacts:
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
