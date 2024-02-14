6f5bdfd038c4b79ff410f3a45b37249213d339d4
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-02-14 10:50:11 +0100

    Add id_verify + range write and read

--- src/Telemetrix4RpiPico.cpp
+++ src/Telemetrix4RpiPico.cpp
@@ -1544,13 +1544,43 @@ void Hiwonder_Servo::writeModule(std::vector<uint8_t> &data) {
   } else if (msg_type == 3) { // update id
     auto new_id = data[1];
     this->bus->id_write(new_id);
+  } else if (msg_type == 4) {
+    // id read
+    auto check_id = data[1];
+    HiwonderServo tempServo(this->bus, check_id);
+    auto ok = tempServo.id_verify();
+    std::vector<uint8_t> data = {
+        4,                  // id check type
+        check_id,           // id
+        ok,                 // ok
+    };
+    this->publishData(data);
+  } else if(msg_type == 5) {
+    // range write
+    auto id = data[1];
+    auto min = ((int32_t)data[2] << 8) | data[3];
+    auto max = ((int32_t)data[4] << 8) | data[5];
+    this->servos[id]->setLimitsTicks(min/24, max/24); // 24 degrees per tick
+  } else if(msg_type == 6) {
+    // read range of servo stored in servo
+    auto id = data[1];
+    this->servos[id]->readLimits();
+    auto min = this->servos[id]->minCentDegrees;
+    auto max = this->servos[id]->maxCentDegrees;
+    std::vector<uint8_t> data = {
+        6,                  // id check type
+        id,           // id
+        (uint8_t)(min >> 8), (uint8_t)(min & 0xff), (uint8_t)(max >> 8), (uint8_t)(max & 0xff)
+    };
+    this->publishData(data);
   }
 }
 
 void Hiwonder_Servo::readModule() {
   // read angle, temp?
   std::vector<uint8_t> data;
-  data.reserve(this->servos.size() * 5);
+  data.reserve(this->servos.size() * 5 +1);
+  data.push_back(0); // message type servo angles
   // only update position when changed
   for (auto i = 0; auto servo : this->servos) {
     auto pos = servo->pos_read();
@@ -1563,7 +1593,7 @@ void Hiwonder_Servo::readModule() {
     servo->lastPublishedPosition = pos;
     i++;
   }
-  if (data.size() > 0) {
+  if (data.size() > 1) {
     this->publishData(data);
   }
 }
