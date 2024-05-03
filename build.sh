496e8e823602e777a86c69aa02e66eb5dd5c0ee9
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-05-03 10:50:10 +0200

    test build err

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ cd build2
 # Sometimes the debug build reports more errors than Release
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
-
+cd ..
+rm -rf build2
+mkdir build2
+cd build2
 # Build for the artifacts:
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
