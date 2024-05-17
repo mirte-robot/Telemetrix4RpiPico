6df7d28c73fa037c561032129de8407c74c977ef
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2024-05-17 15:41:32 +0200

    upd push script to add bin and shasum

--- push.sh
+++ push.sh
@@ -4,6 +4,13 @@ set -xe
 
 mkdir build || true
 cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
 make
+sha256sum Telemetrix4RpiPico.uf2 > Telemetrix4RpiPico.uf2.sha256sum
+sha256sum Telemetrix4RpiPico.bin > Telemetrix4RpiPico.bin.sha256sum
 picotool info Telemetrix4RpiPico.uf2
-sftp mirtefiles@arend-jan.com:/files/telemetrix/release/ <<< $'put Telemetrix4RpiPico.uf2'
+sftp mirtefiles@arend-jan.com:/files/telemetrix/release/ <<END
+put Telemetrix4RpiPico.uf2
+put Telemetrix4RpiPico.uf2.sha256sum
+put Telemetrix4RpiPico.bin.sha256sum
+END
