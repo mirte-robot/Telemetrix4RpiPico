85e80704282ac07e5f586d561a3230ab0874309c
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-07-17 16:39:44 +0200

    Add pico2 support, merge pico1 and pico2 uf2 into one (_merged)

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # de8ae5a
-set -e
+set -ex
 git config --global --add safe.directory /__w/Telemetrix4RpiPico/Telemetrix4RpiPico # fix for cmake not reading git stuff
-mkdir build2/
+mkdir -p build2/
 ls
 cd build2
 # Sometimes the debug build reports more errors than Release
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico ..
 make -j
 cd ..
 # rm -rf build2
@@ -17,3 +17,10 @@ cd ..
 # make
 sha256sum * || true
 sha256sum build/* || true
+
+mkdir -p build_pico2/
+cd build_pico2/
+cmake -DCMAKE_BUILD_TYPE=Release -DPICO_BOARD=pico2 ..
+make -j3
+cd ..
+cat build2/Telemetrix4RpiPico.uf2 build_pico2/Telemetrix4RpiPico.uf2 > build_pico2/Telemetrix4RpiPico_merged.uf2
