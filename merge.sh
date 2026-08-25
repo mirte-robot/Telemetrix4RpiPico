239e180f6a90e266dc303df76d9275fbe941f387
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-08-25 11:36:20 +0200

    Better bootloader switch cmake

--- merge.sh
+++ merge.sh
@@ -2,4 +2,6 @@
 
 set -ex
 ./build_variants.sh
+
+# use w variants to merge so they'll have the led pin fixes
 cat build_pico_w/Telemetrix4RpiPico.uf2 build_pico2_w/Telemetrix4RpiPico.uf2 > build_pico2_w/Telemetrix4RpiPico_merged.uf2
